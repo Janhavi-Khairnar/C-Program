8ed36ad3b5d150f9e8d53ae250a495e654bfde28
Janhavi-Khairnar <khairnarj2000@gmail.com>
2023-02-16 18:34:08 +0530

    [ADDED]:[Janhavi] Using for loop in c

---
+++ Using for loop.c
@@ -0,0 +1,26 @@
+//How to use for loop???
+
+#include<stdio.h>
+int main()
+{
+	int i;
+	for(i = 1 ; i <= 5 ; i++)	
+	{
+		printf("\nHow are you?");
+	}
+	
+//	for(i = 1 ; i<=50 ; i++)
+//	{
+//		if(i%2==0)
+//		printf("\n%d",i);
+//	}
+	
+//	for(i = 1 ; i<=50 ; i++)
+//	{
+//		if(i%2==0)
+//		printf("\n%d",i);
+//	}
+	
+	
+	return 0;
+}
