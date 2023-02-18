4e53b530e7ed01734e4bb939732491dedf681f7f
Janhavi-Khairnar <khairnarj2000@gmail.com>
2023-02-18 12:09:59 +0530

    [ADDED]:[Janahvi]  Using for loop to find factorial of given number in c

---
+++ for loop for factorial.c
@@ -0,0 +1,17 @@
+//Using for loop & find factorials
+
+#include<stdio.h>
+int main()
+{
+	int n,i,fact=1;
+	printf("\nEnter any number :");
+	scanf("%d",&n);
+	
+	for(i=n;i>=1;i--)
+	{
+		fact = fact * i;
+	}
+		printf("\nFactorial=%d",fact);
+	return 0;
+}
+
